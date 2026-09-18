fa954f8f9808f12995a21a73ffeaaad6b4f78d44
Yasmin Macena <yasmacx@gmail.com>
2026-09-18 20:46:10 -0300

    projeto completo

---
+++ program.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "unicsul.h"
+
+int main(int argc, char *argv[])
+{
+    char dt_proc[30];
+    char hr_proc[30];
+    char prox_dt[30];
+
+    int codigo;
+    float valor, vl_mdr, vl_cred;
+    int metodo;
+
+    if (argc != 4)
+    {
+        printf("Quantidade de argumentos invalida.\n");
+        return 1;
+    }
+
+    codigo = atoi(argv[1]);
+    valor = atof(argv[2]);
+    metodo = atoi(argv[3]);
+
+
+    get_data_hora(dt_proc, hr_proc);
+    if(metodo == 0){
+    	get_prox_data(dt_proc, 0, prox_dt);
+    	vl_mdr=0.0;
+    	vl_cred = valor;
+    }else if(metodo == 1){
+    	get_prox_data(dt_proc, 1, prox_dt);
+    	vl_mdr=valor * 0.01;
+    	vl_cred = valor - vl_mdr ;
+    }else{
+    	get_prox_data(dt_proc, 30, prox_dt);
+    	vl_mdr=valor * 0.05;
+    	vl_cred = valor - vl_mdr ;
+    }
+	
+        
+    printf("Data do proc: %s\n", dt_proc);
+    printf("Hora do proc: %s\n", hr_proc);
+    printf("Proxima data util: %s\n", prox_dt);
+
+    printf("Codigo: %d\n", codigo);
+    printf("Valor: %.2f\n", valor);
+    printf("Metodo: %d\n", metodo);
+
+    // gravacao do registro de transacao no formato JSON - PIX - 0
+    printf("<h1>OBRIGADO PELO PAGAMENTO - FACA MAIS VEZES</h1>\n");
+    grava_log(
+        dt_proc,
+        hr_proc,
+        codigo,
+        metodo,
+        valor,
+        vl_mdr,      
+        vl_cred,     
+        prox_dt
+    );
+
+    return 0;
+}
